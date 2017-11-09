99d8ce36eb8c4b2ee3e9282079cc2d108266fe1f
Kendra Kleber <league@Mac-mini-2.local>
2017-11-08 17:53:49 -0800

    Projectiles updated
    The projectiles will now move from one side to another and “die” after
    a certain time amount

--- src/Projectiles.java
+++ src/Projectiles.java
@@ -4,12 +4,28 @@ import java.awt.Graphics;
 public class Projectiles extends GameObject{
 	int speed = 10;
 	int angle = 0;
+	int aliveCountDown=60;
 	void update(){
 		super.update();
+		aliveCountDown--;
+		if(aliveCountDown==0){
+			isAlive=false;
+		}
 		y+=speed*Math.sin(Math.toRadians(angle));
 		x+=speed*Math.cos(Math.toRadians(angle));
-		if(y==0){
-			isAlive=false;
+		
+		if(x<0){
+			System.out.println("turtle");
+			x=800;
+		}
+		else if(x>800){
+			x=0;
+		}
+		if(y<0){
+			y=800;
+		}
+		else if(y>800){
+			y=0;
 		}
 	}
 		void draw(Graphics g){

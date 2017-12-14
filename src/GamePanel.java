2fba751210def451b431a385c03338157dea7c79
tactreconx <league@Admins-Mac-mini-2.local>
2017-12-13 17:55:31 -0800

    Lives complete

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,7 +13,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer t;
 	final int MENU_STATE = 0;
 	final int GAME_STATE = 1;
@@ -24,144 +24,152 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	public static BufferedImage playerImage;
 	public static BufferedImage AstriodImage;
 	ObjectManager om;
-GamePanel(){
-	t = new Timer(1000/60, this);
-	
-	
-	om = new ObjectManager();
-	
-	try{
-		playerImage = ImageIO.read(this.getClass().getResourceAsStream("ship.png"));
-		AstriodImage = ImageIO.read(this.getClass().getResourceAsStream("Astriod.png"));
+
+	GamePanel() {
+		t = new Timer(1000 / 60, this);
+
+		om = new ObjectManager();
+
+		try {
+			playerImage = ImageIO.read(this.getClass().getResourceAsStream("ship.png"));
+			AstriodImage = ImageIO.read(this.getClass().getResourceAsStream("Astriod.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	catch(IOException e){
-		e.printStackTrace();
+
+	void StartGame() {
+		t.start();
+		om.reset();
+		om.makeAstriods();
+		ship = new Rocketship(400, 400, 50, 50);
+		om.addObject(ship);
+		om.setScore(0);
 	}
-}
-void StartGame(){
-	t.start();
-	om.reset();
-	om.makeAstriods();
-	ship = new Rocketship(400,400,50,50);
-	om.addObject(ship);
-	om.setScore(0);
-}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
-		if(currentState == MENU_STATE){
+		if (currentState == MENU_STATE) {
 			updatems();
-		}else if(currentState == GAME_STATE){
+		} else if (currentState == GAME_STATE) {
 			updategs();
-		}else if(currentState == END_STATE){
+		} else if (currentState == END_STATE) {
 			updatees();
 		}
 
 	}
-	void updatems(){
-		
+
+	void updatems() {
+
 	}
-	void updategs(){
+
+	void updategs() {
 		om.update();
-		if(om.numLives== 0){
-			currentState=END_STATE;}
-		else{
+		if (om.numLives == 0) {
+			currentState = END_STATE;
+		}
+		System.out.println(om.numAstriods);
+		if (om.numAstriods == 0) {
+			om.makeAstriods();
+		}
+		if(ship.isAlive==false){
+			ship = new Rocketship(400, 400, 50, 50);
 			om.addObject(ship);
 		}
 	}
-	
-	void updatees(){
-		
+
+	void updatees() {
+
 	}
-	void drawms(Graphics g){
+
+	void drawms(Graphics g) {
 		g.setColor(Color.BLUE);
-		g.fillRect(0, 0,  1000, 1000);
+		g.fillRect(0, 0, 1000, 1000);
 		g.setColor(Color.yellow);
 		g.setFont(titleFont);
 		g.drawString("Totally NOT", 190, 100);
 		g.drawString("Astriods", 190, 135);
 	}
-	void drawgs(Graphics g){
+
+	void drawgs(Graphics g) {
 		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 10000,100000);    
+		g.fillRect(0, 0, 10000, 100000);
 		om.draw(g);
-		System.out.println(om.numAstriods);
-		if(om.numAstriods == 0){
-			om.makeAstriods();
+		g.setColor(Color.magenta);
+		String lives = Integer.toString(om.numLives);
+		g.drawString(lives, 190, 135);
+	}
+
+	void drawes(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, 1000, 1000);
+		g.setColor(Color.BLACK);
+		g.setFont(titleFont);
+		g.drawString("Game", 190, 100);
+		g.drawString("Over :'(", 190, 135);
+	}
+
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU_STATE) {
+			drawms(g);
+		} else if (currentState == GAME_STATE) {
+			drawgs(g);
+		} else if (currentState == END_STATE) {
+			drawes(g);
 		}
 
 	}
-	void drawes(Graphics g){
-	g.setColor(Color.RED);
-	g.fillRect(0, 0, 1000, 1000);
-	g.setColor(Color.BLACK);
-	g.setFont(titleFont);
-	g.drawString("Game", 190, 100);
-	g.drawString("Over :'(", 190, 135);
-	}
-public void paintComponent(Graphics g){
-	if(currentState == MENU_STATE){
-		drawms(g);
-	}else if(currentState == GAME_STATE){
-		drawgs(g);
-	}else if(currentState == END_STATE){
-		drawes(g);
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("pizza");
 	}
 
-	}
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("pizza");
-}
-@Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("bob");
-	if(e.getKeyCode()==KeyEvent.VK_ENTER){
-		currentState++;
-		if(currentState == 1){
-			StartGame();
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("bob");
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if(currentState == MENU_STATE){
+				currentState = GAME_STATE;
+				StartGame();
+			}
+			else if (currentState == END_STATE){
+				currentState = MENU_STATE;
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			ship.aspeed = 5;
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			ship.aspeed = -5;
+		} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+			ship.speed = 5;
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			ship.speed = -5;
+		} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			if (ship.isAlive) {
+				om.addObject(new Projectiles(ship.x + ship.width / 2, ship.y, 10, 10, ship.angle));
+			}
 		}
 	}
-	if(currentState > END_STATE){
-		currentState = MENU_STATE;
-	}
-	if(e.getKeyCode()==KeyEvent.VK_LEFT){
-		ship.aspeed=5;
-	}
-	else if (e.getKeyCode()== KeyEvent.VK_RIGHT){
-		ship.aspeed=-5;
-	}
-	else if(e.getKeyCode()==KeyEvent.VK_UP){
-		ship.speed=5;
-	}
-	else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-		ship.speed=-5;
-	}
-else if (e.getKeyCode()==KeyEvent.VK_SPACE){
-	om.addObject(new Projectiles(ship.x+ship.width/2, ship.y, 10, 10, ship.angle));
-	}
 
-}
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("chicken");
-	if(e.getKeyCode()==KeyEvent.VK_LEFT){
-		ship.aspeed=0;
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("chicken");
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			ship.aspeed = 0;
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			ship.aspeed = 0;
+		} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+			ship.speed = 0;
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			ship.speed = 0;
+		}
+
 	}
-	else if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-		ship.aspeed=0;
-	}
-	else if(e.getKeyCode()==KeyEvent.VK_UP){
-		ship.speed=0;
-	}
-	else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-		ship.speed=0;
-	}
-	
-}
 
 }
